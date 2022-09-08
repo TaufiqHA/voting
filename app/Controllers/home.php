8d5b7896bba99a2521a3fedc517bfe80beb01dc8
TaufiqHA <htaufiq225@gmail.com>
2022-09-08 21:53:23 +0700

    finish

--- app/Controllers/home.php
+++ app/Controllers/home.php
@@ -12,8 +12,23 @@ class Home extends Controller
 		$this->view('templates/footer');
 	}
 
+	public function thanks()
+	{
+		echo 'taufiq hidayah abdullah';
+	}
+
 	public function get()
 	{
 		echo json_encode($data['nilai'] = $this->model('Voting_model')->getData());
+	}
+
+	public function tambahVoting($id, $nilai)
+	{
+		$data['id'] = $id;
+		$data['nilai'] = $nilai;
+		$vote = $this->model('Voting_model')->tambahVote($data);
+		if ($vote === true) {
+			$this->thanks();
+		}
 	}
 }
--- app/Models/Voting_model.php
+++ app/Models/Voting_model.php
@@ -13,4 +13,13 @@ class Voting_model
 		$this->db->query('SELECT * FROM vote');
 		return $this->db->resultset();
 	}
+
+	public function tambahVote($data)
+	{
+		$this->db->query('UPDATE vote SET nilai = nilai + :nilai WHERE id = :id');
+		$this->db->bind('id', $data['id']);
+		$this->db->bind('nilai', $data['nilai']);
+		$this->db->execute();
+		return true;
+	}
 }
